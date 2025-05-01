79203601722eff7e2b93128f6d0fe861bae24497
Dmitry Holodov <dimalinux@protonmail.com>
2025-04-30 19:58:47 -0700

    dalek_ff_group simplifications

--- helioselene-contest/helioselene-contest-src/src/lib.rs
+++ helioselene-contest/helioselene-contest-src/src/lib.rs
@@ -1,19 +1,14 @@
 #![no_std]
 
-//pub use group;
-
-#[macro_use]
 mod backend;
-
-mod field;
-pub use field::HelioseleneField;
-
 mod bigint;
 mod dalek_ff_group;
+mod field;
 mod helios;
 mod point;
 mod selene;
 
 pub use dalek_ff_group::Field25519;
+pub use field::HelioseleneField;
 pub use group;
 pub use point::{HeliosPoint, SelenePoint};
